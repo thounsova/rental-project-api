5752c049a194c917db8e6ea4c921c4d3b75745fe
Roeurnvanneth <roeurn.vanneth@institute.pse.ngo>
2025-12-26 08:48:16 +0700

    This Entity for rejister

---
+++ src/main/java/com/Rental/rental/Dtos/ResponseDto/RegisterResponseDto.java
@@ -0,0 +1,14 @@
+package com.Rental.rental.Dtos.ResponseDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterResponseDto {
+
+    private String message;   // Success message
+    private String username;  // Optional: the registered username
+}
